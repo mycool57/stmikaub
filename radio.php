e2b2c81f098c537cfba117580497dcae8189174a
mycooll57@gmail.com <mycooll57@gmail.com>
2016-12-06 04:32:50 +0700

    t

---
+++ radio.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<body>
+<body align="center" ><form action="/index.php">
+    <input type="submit" value="RADIO V1" />
+</form><br><div id="cp_widget_579341a2-933b-430e-92cf-4fc3e8953ae8">...</div><script type="text/javascript">
+var cpo = []; cpo["_object"] ="cp_widget_579341a2-933b-430e-92cf-4fc3e8953ae8"; cpo["_fid"] = "AADAcxN2qHnL";
+var _cpmp = _cpmp || []; _cpmp.push(cpo);
+(function() { var cp = document.createElement("script"); cp.type = "text/javascript";
+cp.async = true; cp.src = "//www.cincopa.com/media-platform/runtime/libasync.js";
+var c = document.getElementsByTagName("script")[0];
+c.parentNode.insertBefore(cp, c); })(); </script><noscript>Powered by Cincopa <a href='https://www.cincopa.com/media-platform/skins/responsive-video-player-with-horizontal-playlist'>Video Playlist</a> for Business solution.</noscript>
+</body>
+</body>
+</html>
